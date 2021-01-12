96d71aec9d8a3ac4ce0d29ffbdc56684e001cdba
ErlensGadzans <erlens.gadzans@users.noreply.github.com>
2021-01-12 17:06:34 +0200

    get method &get method by id is added

--- src/articles/index.js
+++ src/articles/index.js
@@ -4,7 +4,26 @@ const ArticleModel = require("./schema.js");
 const articlesRouter = express.Router();
 
 // GET /articles => returns the list of articles
+articlesRouter.get("/", async (req, res, next) => {
+  try {
+    const articles = await ArticleModel.find();
+    res.send(articles);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 // GET /articles/:id => returns a single article
+articlesRouter.get("/:id", async (req, res, next) => {
+  try {
+    const article = await ArticleModel.findById(req.params.id);
+    res.send(article);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
 // POST /articles => create a new article
 articlesRouter.post("/", async (req, res, next) => {
   try {
